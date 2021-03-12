75b0fb4ccf612ce90882dcdc1d2592896c27acd8
Divanio Silva <dcsilva@mail.ru>
2021-03-12 10:20:36 +0000

    First commit

---
+++ src/test/java/com/ds/spring_rabbit_mq/SpringRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.spring_rabbit_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
